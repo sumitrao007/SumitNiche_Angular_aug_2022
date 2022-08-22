6e7d102769b9d8895aa21ee720217ef34a79985b
Raokhande sumit Ashok <raokhande.sumit@gmail.com>
2022-08-22 21:49:28 +0530

    Day03

---
+++ Typescript/Day03/demo1.ts
@@ -0,0 +1,81 @@
+
+// console.log("Hello world ");
+
+// var a1;
+// var a2,a3;
+
+// data types
+// number(int,long,float,double,short),string(''/""),boolean(true/false)
+// any => (number,string,boolean)
+// void => it is neutral data type => it doies not point anything
+// misc. null & undefine => it works as data types as well as value
+
+var a:number=10;  //forward declaration & defination
+// console.log("Value of a "+a);
+
+var a1:string;
+// console.log(`Value of a1 ${a1}`)
+var a2;
+// console.log(`Value of a2 is ${a2}`)
+
+var a3=2.5;
+// console.log("data type of a3 "+typeof a3)
+// a3='sumit'; // it is not possible
+
+var a4:any;
+
+a4=3.14;
+// console.log(`Value of a4 is ${a4}`)
+a4='sumit';
+// console.log(`Value of a4 is ${a4}`) 
+a4=true;
+// console.log(`Value of a4 is ${a4}`)
+
+//Type assertion
+
+//1 angle bracket
+//2 as syntax
+
+//1 angle bracket
+
+var b:any;
+
+var temp=(<string> b);
+// temp.
+
+//2 as syntax
+
+var temp2=(b as string);
+
+// temp.
+
+//operators
+//Airthnmatic => +,-,*,/,%
+//logical => &&,||
+// bitwise => &,|,!,~,>>,<<,^
+// assignment => =,!=,==(it checks only value),===(it checks value as well as data type=> Strongly equality),+=,-=,/=,*=
+// unary => post/pre inc/dec ++/--
+// ternary => condition?expression1:express2
+// typeof
+// Relational => <,>,<=,>=,!=
+
+// control statement
+// if ,if-else,switch,if-else ladder,break,continue
+
+  // if(21<4){
+  //   console.log("Condition is true")
+  // }else{
+  //   console.log("Condition false")
+  // }
+
+var choice=21;
+
+switch(choice){
+  case 1:console.log("U r case 1");
+          break;
+  case 2: console.log("u r in case 2");
+        break;
+  default:console.log("U r in default case");
+        break;
+}
+
