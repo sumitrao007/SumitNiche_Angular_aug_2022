80b0b8be946d86971b25b9fe4c05aa765ab0a861
Raokhande sumit Ashok <raokhande.sumit@gmail.com>
2022-09-21 21:51:05 +0530

    Day11

---
+++ Angular/Day11/inbuildpipe/inbuildpipe.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inbuildpipe',
+  templateUrl: './inbuildpipe.component.html',
+  styleUrls: ['./inbuildpipe.component.css']
+})
+export class InbuildpipeComponent implements OnInit {
+
+    strdata:string='Hopes so u all are Enjoying angular 12 Module from Sumit   raokhande';
+
+    num2:number=-452.78956324557;
+
+    mydate=new Date();
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
