c93a577f4e4206d0bdc35eb78cabf9883c899dce
Raokhande sumit Ashok <raokhande.sumit@gmail.com>
2022-09-30 21:44:57 +0530

    Day17

---
+++ Angular/Day17/queryparameter/queryparameter.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-queryparameter',
+  templateUrl: './queryparameter.component.html',
+  styleUrls: ['./queryparameter.component.css']
+})
+export class QueryparameterComponent implements OnInit {
+
+  constructor(private route:ActivatedRoute) { }
+
+  ngOnInit(): void {
+      this.GetParameterFromUrl();
+  }
+  GetParameterFromUrl(){
+      this.route.queryParamMap
+      .subscribe((param)=>{
+        console.log(param.get('id'))
+        console.log(param.get('fname'))
+        console.log(param.get('status'))
+      })
+  }
+
+}
